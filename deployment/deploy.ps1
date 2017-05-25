2727947b278b82b4ce6af9ed56962abb5267e96f
John Manaloto <jomanalo@microsoft.com>
2017-05-25 16:46:05 -0700

    deployment tweaks

--- deployment/deploy.ps1
+++ deployment/deploy.ps1
@@ -1,13 +1,26 @@
+
+
+# create resource group
+$resourceGroupName = 'SwimLanes'
+$location = 'west us'
+Get-AzureRmResourceGroup -Name $resourceGroupName -ev notPresent -ea 0
+
+if ($notPresent)
+{
+    new-azurermresourcegroup -Name $resourceGroupName -Location $location
+}
+
+
 # deploy input storage account
 
 # deploy function(s)
-New-AzureRmResourceGroupDeployment -Name [deployment name] -ResourceGroupName [resource-group-name] `
+New-AzureRmResourceGroupDeployment -Name $resourceGroupName -ResourceGroupName $resourceGroupName `
     -Mode Incremental -Verbose `
     -TemplateFile "functions.azuredeploy.json" `
     -TemplateParameterFile "functions.parameters.json"
 
 # deploy sql database
-New-AzureRmResourceGroupDeployment -Name [deployment name] -ResourceGroupName [resource-group-name] `
+New-AzureRmResourceGroupDeployment -Name $resourceGroupName -ResourceGroupName $resourceGroupName `
     -Mode Incremental -Verbose `
     -TemplateFile "database.azuredeploy.json" `
     -TemplateParameterFile "database.parameters.json"
