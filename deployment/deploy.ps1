b6aa1b41dde5d86c4dcd1dd2cd3bccc5a0095055
John Manaloto <jomanalo@microsoft.com>
2017-05-25 16:05:09 -0700

    rename files

--- deployment/deploy.ps1
+++ deployment/deploy.ps1
@@ -1,4 +1,9 @@
+# deploy input storage account
+
+# deploy function(s)
 New-AzureRmResourceGroupDeployment -Name [deployment name] -ResourceGroupName [resource-group-name] `
     -Mode Incremental -Verbose `
-    -TemplateFile "azuredeploy.json" `
-    -TemplateParameterFile "azuredeploy.parameters.json"
+    -TemplateFile "functions.azuredeploy.json" `
+    -TemplateParameterFile "functions.parameters.json"
+
+# deploy sql database
