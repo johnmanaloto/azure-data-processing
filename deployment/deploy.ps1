913eea1cfda137d729b25a962cde41e2eb752826
John Manaloto <jomanalo@microsoft.com>
2017-06-02 11:29:52 -0700

    consolidiated arm templates

--- deployment/deploy.ps1
+++ deployment/deploy.ps1
@@ -1,30 +1,83 @@
-
-
 # create resource group
 $resourceGroupName = 'SwimLanes'
 $location = 'west us'
-Get-AzureRmResourceGroup -Name $resourceGroupName -ev notPresent -ea 0
+$inputDataContainerName = 'input-data'
+$miscContainerName = 'misc'
 
-if ($notPresent)
+$dbSchemaDB = "..\src\SqlDatabase\CustomerAndAddressTables.sql" 
+
+Write-Host ""
+Write-Host "**************************************************************************************************"
+Write-Host "* Create resource group..."
+Write-Host "**************************************************************************************************"
+
+Get-AzureRmResourceGroup -Name $resourceGroupName -ev resourceGroupNotFound -ea 0
+
+if ($resourceGroupNotFound)
 {
     new-azurermresourcegroup -Name $resourceGroupName -Location $location
+	Write-Host("Resource group '$resourceGroupName' created")
+}
+else
+{
+	Write-Host("Resource group '$resourceGroupName' already exists")
+}
+
+Write-Host ""
+Write-Host "**************************************************************************************************"
+Write-Host "* Deploy ARM template..."
+Write-Host "**************************************************************************************************"
+
+$deployment = New-AzureRmResourceGroupDeployment -Name $resourceGroupName -ResourceGroupName $resourceGroupName `
+    -Mode Incremental -Verbose `
+    -TemplateFile "azuredeploy.json" `
+    -TemplateParameterFile "azuredeploy.parameters.json"
+
+Write-Host "Template deployment complete"
+
+Write-Host ""
+Write-Host "**************************************************************************************************"
+Write-Host "* Create storage account containers..."
+Write-Host "**************************************************************************************************"
+
+Set-AzureRmCurrentStorageAccount -ResourceGroupName $resourceGroupName -Name $deployment.Outputs.inputStorageAccountName.Value
+Get-AzureStorageContainer -Name $inputDataContainerName -EV inputDataContainerNotFound -EA 0
+
+if ($inputDataContainerNotFound)
+{
+    New-AzureStorageContainer -Name $inputDataContainerName
+	Write-Host("Container '$inputDataContainerName' created")
+}
+else
+{
+    write-host("Container already exists")
+	Write-Host("Container '$inputDataContainerName' already exists")
+}
+
+Get-AzureStorageContainer -Name $miscContainerName -EV miscContainerNotFound -EA 0
+
+if ($miscContainerNotFound)
+{
+    New-AzureStorageContainer -Name $miscContainerName
+	Write-Host("Container '$miscContainerName' created")
+}
+else
+{
+    write-host("Container already exists")
+	Write-Host("Container '$miscContainerName' already exists")
 }
 
 
-# deploy input storage account
-New-AzureRmResourceGroupDeployment -Name $resourceGroupName -ResourceGroupName $resourceGroupName `
-    -Mode Incremental -Verbose `
-    -TemplateFile "storageaccounts.azuredeploy.json" `
-    -TemplateParameterFile "storageaccounts.parameters.json"
+Write-Host ""
+Write-Host "**************************************************************************************************"
+Write-Host "* Initialize target database tables..."
+Write-Host "**************************************************************************************************"
 
-# deploy function(s)
-New-AzureRmResourceGroupDeployment -Name $resourceGroupName -ResourceGroupName $resourceGroupName `
-    -Mode Incremental -Verbose `
-    -TemplateFile "functions.azuredeploy.json" `
-    -TemplateParameterFile "functions.parameters.json"
-
-# deploy sql database
-New-AzureRmResourceGroupDeployment -Name $resourceGroupName -ResourceGroupName $resourceGroupName `
-    -Mode Incremental -Verbose `
-    -TemplateFile "database.azuredeploy.json" `
-    -TemplateParameterFile "database.parameters.json"
+$databaseName = $deployment.Outputs.sqlDBName.Value
+Write-Host "Initializing the '$databaseName' database..."
+.\setupDb.ps1 -ServerName $deployment.Outputs.sqlServerFullyQualifiedDomainName.Value `
+						-AdminLogin $deployment.Outputs.sqlServerAdminLogin.Value `
+						-AdminPassword $deployment.Outputs.sqlServerAdminPassword.Value `
+						-DatabaseName $deployment.Outputs.sqlDBName.Value `
+						-ScriptPath $dbSchemaDB
+Write-Host "'$databaseName' initialized"
