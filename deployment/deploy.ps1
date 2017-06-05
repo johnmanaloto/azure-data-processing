dca01e8257fed6a2bebde5c0365c30cb2e7dc915
John Manaloto <jomanalo@microsoft.com>
2017-06-04 17:56:59 -0700

    Azure AD application and service principal
    Added Azure AD application and service principal creation and integrated with Azure function app settings

--- deployment/deploy.ps1
+++ deployment/deploy.ps1
@@ -6,7 +6,10 @@
 Param(
    [string] [Parameter(Mandatory=$true)] $resourceGroupName,
    [string] [Parameter(Mandatory=$true)] $location,
+   [string] [Parameter(Mandatory=$false)] $applicationName = 'AutomationAdApp',
    [string] [Parameter(Mandatory=$false)] $inputDataContainerName = 'input-data',
+   [string] [Parameter(Mandatory=$false)] $outputDataContainerName = 'misc',
+   [string] [Parameter(Mandatory=$false)] $usqlDataContainerName = 'misc',
    [string] [Parameter(Mandatory=$false)] $miscContainerName = 'misc'
 )
 
@@ -31,13 +34,56 @@ else
 
 Write-Host ""
 Write-Host "**************************************************************************************************"
+Write-Host "* Create Azure AD application..."
+Write-Host "**************************************************************************************************"
+
+# define Azure AD application credentials
+$applicationUri = "http://" + $applicationName
+$KeyValue = [guid]::NewGuid()
+$creds = New-Object Microsoft.Azure.Commands.Resources.Models.ActiveDirectory.PSADPasswordCredential
+$startDate = Get-Date
+$creds.StartDate = $startDate
+$creds.EndDate = $startDate.AddYears(1)
+$creds.KeyId = [guid]::NewGuid()
+$creds.Password = $KeyValue
+
+write-host $applicationUri
+
+# create Azure AD application (delete it first if it already exists)
+$app = get-azurermadapplication -IdentifierUri "http://$applicationName"
+if ($app){
+    remove-azurermadapplication -ObjectId $app.ObjectId -Force
+}
+
+$app = New-AzureRmADApplication –DisplayName $applicationName `
+-IdentifierUris ("http://" + $applicationName) `
+-PasswordCredentials $creds
+
+# create service principal
+New-AzureRmADServicePrincipal -ApplicationId $app.ApplicationId
+$princ = Get-AzureRmADServicePrincipal -SearchString $applicationName
+# sleep for 30 seconds to allow service principal time to get created
+Start-Sleep -s 15
+
+# assign Contributor role at subscription level
+$sub = get-azurermcontext | select-object Subscription
+$scope = "/subscriptions/" + $sub.Subscription
+New-AzureRmRoleAssignment -RoleDefinitionName Contributor -ObjectId $princ.Id -scope $scope
+
+$displayName = $app.DisplayName
+Write-Output "The application '$displayName' with secret '$keyValue' has been created/re-created."
+
+Write-Host ""
+Write-Host "**************************************************************************************************"
 Write-Host "* Deploy ARM template..."
 Write-Host "**************************************************************************************************"
 
 $deployment = New-AzureRmResourceGroupDeployment -Name $resourceGroupName -ResourceGroupName $resourceGroupName `
     -Mode Incremental -Verbose `
     -TemplateFile "azuredeploy.json" `
-    -TemplateParameterFile "azuredeploy.parameters.json"
+    -TemplateParameterFile "azuredeploy.parameters.json" `
+	-clientId $app.ApplicationId `
+	-clientSecret $keyValue
 
 Write-Host "Template deployment complete"
 
@@ -52,12 +98,35 @@ Get-AzureStorageContainer -Name $inputDataContainerName -EV inputDataContainerNo
 if ($inputDataContainerNotFound)
 {
     New-AzureStorageContainer -Name $inputDataContainerName
-	Write-Host("Container '$inputDataContainerName' created")
+	Write-Host("Container $inputDataContainerName created")
 }
 else
 {
-    write-host("Container already exists")
-	Write-Host("Container '$inputDataContainerName' already exists")
+    write-host("$inputDataContainerName container already exists")
+}
+
+Get-AzureStorageContainer -Name $outputDataContainerName -EV outputDataContainerNotFound -EA 0
+
+if ($outputDataContainerNotFound)
+{
+    New-AzureStorageContainer -Name $outputDataContainerName
+	Write-Host("Container $outputDataContainerName created")
+}
+else
+{
+    write-host("$outputDataContainerName Container already exists")
+}
+
+Get-AzureStorageContainer -Name $usqlDataContainerName -EV usqlDataContainerNotFound -EA 0
+
+if ($usqlDataContainerNotFound)
+{
+    New-AzureStorageContainer -Name $usqlDataContainerName
+	Write-Host("Container $usqlDataContainerName created")
+}
+else
+{
+    write-host("$usqlDataContainerName container already exists")
 }
 
 Get-AzureStorageContainer -Name $miscContainerName -EV miscContainerNotFound -EA 0
@@ -65,12 +134,11 @@ Get-AzureStorageContainer -Name $miscContainerName -EV miscContainerNotFound -EA
 if ($miscContainerNotFound)
 {
     New-AzureStorageContainer -Name $miscContainerName
-	Write-Host("Container '$miscContainerName' created")
+	Write-Host("Container $miscContainerName created")
 }
 else
 {
-    write-host("Container already exists")
-	Write-Host("Container '$miscContainerName' already exists")
+	Write-Host("$miscContainerName container already exists")
 }
 
 
